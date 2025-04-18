be02408d4b740f640ed77f9fe1413ffd17f48b63
Moseco <hampus_hammarlund@hotmail.com>
2025-04-18 21:06:17 +0900

    fix for flutter 3.29

--- android/src/main/kotlin/com/piccmaq/disk_space_plus/DiskSpacePlusPlugin.kt
+++ android/src/main/kotlin/com/piccmaq/disk_space_plus/DiskSpacePlusPlugin.kt
@@ -1,32 +1,64 @@
 package com.piccmaq.disk_space_plus
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+import android.os.Environment
+import android.os.StatFs
 
-class DiskSpacePlusPlugin: FlutterPlugin {
+class DiskSpacePlusPlugin: FlutterPlugin, MethodCallHandler {
+  private lateinit var channel : MethodChannel
 
-  companion object {
-    private var channel: MethodChannel? = null
-    private var handler: MethodHandlerImpl = MethodHandlerImpl()
-
-    @JvmStatic
-    fun registerWith(registrar: PluginRegistry.Registrar) {
-      registerChannel(registrar.messenger())
-    }
-
-    private fun registerChannel(messenger: BinaryMessenger) {
-      channel = MethodChannel(messenger, "disk_space_plus")
-      channel!!.setMethodCallHandler(handler)
-    }
+  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "disk_space_plus")
+    channel.setMethodCallHandler(this)
   }
 
-  override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    registerChannel(binding.binaryMessenger)
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    when(call.method) {
+        "getFreeDiskSpace" -> result.success(getFreeDiskSpace())
+        "getTotalDiskSpace" -> result.success(getTotalDiskSpace())
+        "getFreeDiskSpaceForPath" -> result.success(getFreeDiskSpaceForPath(call.argument<String>("path")!!))
+        else -> result.notImplemented()
+    }
   }
 
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    channel = null
+    channel.setMethodCallHandler(null)
+  }
+
+  private fun getFreeDiskSpace(): Double {
+      val stat = StatFs(Environment.getExternalStorageDirectory().path)
+
+      val bytesAvailable: Long
+      bytesAvailable = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2)
+          stat.blockSizeLong * stat.availableBlocksLong
+      else
+          stat.blockSize.toLong() * stat.availableBlocks.toLong()
+      return (bytesAvailable / (1024f * 1024f)).toDouble()
+  }
+
+  private fun getFreeDiskSpaceForPath(path: String): Double {
+      val stat = StatFs(path)
+  
+      val bytesAvailable: Long
+      bytesAvailable = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2)
+          stat.blockSizeLong * stat.availableBlocksLong
+      else
+          stat.blockSize.toLong() * stat.availableBlocks.toLong()
+      return (bytesAvailable / (1024f * 1024f)).toDouble()
+  }
+
+  private fun getTotalDiskSpace(): Double {
+      val stat = StatFs(Environment.getExternalStorageDirectory().path)
+
+      val bytesAvailable: Long
+      bytesAvailable = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2)
+          stat.blockSizeLong * stat.blockCountLong
+      else
+          stat.blockSize.toLong() * stat.blockCount.toLong()
+      return (bytesAvailable / (1024f * 1024f)).toDouble()
   }
 }
